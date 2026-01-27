06ea39772093d07bc27046f60a23b8ae3ef6f6cd
Anton Lindberg <antonlindberg000@gmail.com>
2026-01-27 15:31:46 +0100

    Full far-field simulations polar plot and frequency plot now work. Not 100% on the normalization and 90deg broadfire angle. TODO check if we use the correct angles et. I use phi for angle in simulation and theta0 for steering angle, this is not consistent with literature i dont think. It usually is theta and theta0

--- Arrays.m
+++ Arrays.m
@@ -1,31 +1,71 @@
-clear; clc;
-
+clear all; clc;
+%%
 N = 5;
 d = 0.05;
-x = ((0:N-1) - (N-1)/2) * d;   % element positions (row)
+x = ((0:N-1) - (N-1)/2).' * d;
 f = 1000;
-c = 343;
+c = 343; 
 k = 2*pi*f/c;
-
+theta0 = deg2rad(90);              %TARGETANGLE
 phi_deg = 0:0.25:180;
-phi = deg2rad(phi_deg);
+phi = deg2rad(phi_deg);  
+%%
+SF = exp(-1j*k*x.*cos(theta0));    %SF - STEERFACTOR
+TF = taper_uniform(N);             %TF - TAPERFACTOR
+W = TF .* SF;                      %W - WEIGHTS
 
-w = taper_uniform(N);          % make sure this returns a ROW vector 1xN
+LV = exp(1j * k * (x * cos(phi))); %LV - LOOKING VECTOR
 
-% Steering vector for every phi -> N x numAngles
-SV = exp(1j*k*(x.').*cos(phi));    % x' is Nx1, cos(phi) is 1xM => NxM
-
-AF = w' * SV;                   % (1xN)*(NxM) = 1xM
-mag = abs(AF);
-mag = mag / max(mag);          % normalize (optional)
+AF = W.' * LV;
+AF_mag = abs(AF);
+AF_mag = AF_mag / max(AF_mag);  % normalize
 
 figure;
-polarplot(phi, mag, "LineWidth", 2);
-
+polarplot(phi, AF_mag, 'LineWidth', 2);
+title('Array Factor |A(\theta)|');
 
 %%
+freqs = 200:20:4000;
+B = zeros(numel(freqs), numel(phi));
+TF = taper_uniform(N);
 
+for fi = 1:numel(freqs)
+    f = freqs(fi);
+    k = 2*pi*f/c;
+    SF = exp(-1j*k*x.*cos(theta0));
+    W = TF .* SF;                      %W - WEIGHTS
+    LV = exp(1j * k * (x * cos(phi))); %LV - LOOKING VECTOR
 
+    AF = W.' * LV;
+    AF_mag = abs(AF);
+    B(fi,:) = AF_mag / max(AF_mag + eps);
+end
+figure;
+imagesc(phi_deg, freqs/1000, B);   % x=phi in degrees, y=f in kHz
+axis xy;
+%
+xlabel('\phi (deg)  (90° = broadside)');
+ylabel('Frequency (kHz)');
+colormap parula;
+colorbar;
+%%
+a=taper_uniform(N)
+N=10
+b=taper_binomial(N)
+%%
+
+N_levels = [5, 7];
+d_levels = [0.10, 0.02];
+
+x = make_nested_array(N_levels, d_levels);
+
+figure;
+stem(x, ones(size(x)), 'filled');
+xlabel('x [m]');
+title('Nested linear array');
+grid on;
+
+%%
 function x = make_nested_array(N_levels, d_levels)
     assert(numel(N_levels) == numel(d_levels), ...
         'N_levels and d_levels must have same length');
@@ -54,21 +94,3 @@ function w = taper_binomial(N)
     w = double(w);
     w = w ./ sum(w);
 end
-
-
-%%
-a=taper_uniform(N)
-N=10
-b=taper_binomial(N)
-%%
-
-N_levels = [5, 7];
-d_levels = [0.10, 0.02];
-
-x = make_nested_array(N_levels, d_levels);
-
-figure;
-stem(x, ones(size(x)), 'filled');
-xlabel('x [m]');
-title('Nested linear array');
-grid on;
